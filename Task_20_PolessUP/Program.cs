4af1f5df01084d877de4aab0219400039268aae2
Zusith <haifi2@mail.ru>
2023-04-15 16:05:02 +0300

    Добавьте файлы проекта.

---
+++ Task_20_PolessUP/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = 11;
+Console.WriteLine("Ввод: " + n);
+Console.WriteLine("Вывод(количество простых чисел < n): " + PrimesNumberLessThen(n)); 
+
+int PrimesNumberLessThen(int n) 
+{
+    var numbers = new List<int>();
+    for (int i = 2; i < n; i++)
+    {
+        numbers.Add(i);
+    }
+
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        for (int j = 2; j < n; j++)
+        {
+            numbers.Remove(numbers[i] * j);
+        }
+    }
+    return numbers.Count;
+}
+
+
